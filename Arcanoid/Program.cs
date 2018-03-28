39f286dbe35365d236b585ed3f113909b90aede0
Olexiy Kocherga <carloselpibevalderrama@gmail.com>
2018-03-28 15:31:16 +0300

    Добавьте файлы проекта.

---
+++ Arcanoid/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcanoid
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+       
+                int x = 0, y = 0, offsetX = 1, offsetY = 1;
+                Console.CursorVisible = false;
+                while (true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        switch (Console.ReadKey().Key)
+                        {
+                            case ConsoleKey.Escape:
+                                goto End;
+                        }
+                    }
+                    Console.CursorLeft = x;
+                    Console.CursorTop = y;
+                    Console.Write("0");
+                    x += offsetX;
+                    y += offsetY;
+                    if (x < 0)
+                    {
+                        x = 0;
+                        offsetX = -offsetX;
+                    }
+                    else if (x > 40)
+                    {
+                        x = 40;
+                        offsetX = -offsetX;
+                    }
+                    else if (y < 0)
+                    {
+                        y = 0;
+                        offsetY = -offsetY;
+                    }
+                    else if (y > 40)
+                    {
+                        y = 40;
+                        offsetY = -offsetY;
+                    }
+                }
+        End:;
+            }
+        }
+    }
